82e9a8c22e1ed0fbf93eb4f51523443a18901f21
Junyang Qian <jyqian@JunyangQiantekiMacBook-Pro.local>
2019-01-21 22:12:44 -0800

    Accommodate big matrix in glmnet fit

--- R/elnet.R
+++ R/elnet.R
@@ -33,36 +33,43 @@ if(nulldev==0)stop("y is constant; gaussian glmnet fails at standardization step
   }
 
   if (!isg) plam <- -1.0
-  
+
   if (!is.sparse && is.null(beta0)) {
     beta0 <- rep(0, ncol(x))
   }
-  
-fit=if(is.sparse).Fortran("spelnet",
-        ka,parm=alpha,nobs,nvars,x,ix,jx,y,weights,jd,vp,cl,ne,nx,nlam,flmin,ulam,thresh,isd,intr,maxit,
-        lmu=integer(1),
-        a0=double(nlam),
-        ca=double(nx*nlam),
-        ia=integer(nx),
-        nin=integer(nlam),
-        rsq=double(nlam),
-        alm=double(nlam),
-        nlp=integer(1),
-        jerr=integer(1),PACKAGE="glmnetPlus"
-        )
-else .Fortran("elnet",
-          ka,parm=alpha,nobs,nvars,as.double(x),y,weights,jd,vp,cl,ne,nx,nlam,flmin,ulam,thresh,isd,intr,maxit,
-          beta0,isg,plam,
-          lmu=integer(1),
-          a0=double(nlam),
-          ca=double(nx*nlam),
-          ia=integer(nx),
-          nin=integer(nlam),
-          rsq=double(nlam),
-          alm=double(nlam),
-          nlp=integer(1),
-          jerr=integer(1),PACKAGE="glmnetPlus"
-          )
+
+  fit = if(is.sparse) {
+    .C64("spelnet",
+         SIGNATURE = c("integer", "double", "integer", "integer", "double", "integer", "integer", "double",
+                       "double", "integer", "double", "double", "integer", "integer", "integer", "double",
+                       "double", "double", "integer", "integer", "integer",
+                       "integer", "double", "double", "integer", "integer",
+                       "double", "double", "integer", "integer"),
+         ka, parm = alpha, nobs, nvars, x, ix, jx, y,
+         weights, jd, vp, cl, ne, nx, nlam, flmin,
+         ulam, thresh, isd, intr, maxit,
+         lmu = integer(1), a0 = double(nlam), ca = double(nx*nlam), ia = integer(nx), nin = integer(nlam),
+         rsq = double(nlam), alm = double(nlam), nlp = integer(1), jerr = integer(1),
+         INTENT = c(rep("r", 21), rep("w", 9)),
+         PACKAGE = "glmnetPlus"
+    )
+  } else {
+    .C64("elnet",
+         SIGNATURE = c("integer", "double", "integer", "integer", "double", "double",
+                       "double", "integer", "double", "double", "integer", "integer", "integer", "double",
+                       "double", "double", "integer", "integer", "integer", "double",
+                       "integer", "double", "double", "integer", "integer",
+                       "double", "double", "integer", "integer", "integer", "double"),
+         ka, parm = alpha, nobs, nvars, as.double(x), y,
+         weights, jd, vp, cl, ne, nx, nlam, flmin,
+         ulam, thresh, isd, intr, maxit, beta0, isg, plam,
+         lmu = integer(1), a0 = double(nlam), ca = double(nx*nlam), ia = integer(nx), nin = integer(nlam),
+         rsq = double(nlam), alm = double(nlam), nlp = integer(1), jerr = integer(1),
+         INTENT = c(rep("r", 22), rep("w", 9)),
+         PACKAGE = "glmnetPlus"
+    )
+  }
+
 if(fit$jerr!=0){
   errmsg=jerr(fit$jerr,maxit,pmax=nx,family="gaussian")
   if(errmsg$fatal)stop(errmsg$msg,call.=FALSE)
