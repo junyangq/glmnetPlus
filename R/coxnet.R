38072a4eb8ef24a45ff93d91eff8780d11b46bd9
Junyang Qian <qianjunyang@gmail.com>
2019-12-03 23:45:29 -0800

    Enable coxnet to take large X matrix

--- R/coxnet.R
+++ R/coxnet.R
@@ -13,19 +13,35 @@ coxnet=function(x,is.sparse,ix,jx,y,weights,offset,alpha,nobs,nvars,jd,vp,cl,ne,
     is.offset=TRUE
   }
   fit=if(is.sparse)stop("Cox model mot implemented for sparse x in glmnet",call.=FALSE)
-  else .Fortran("coxnet",
-              parm=alpha,nobs,nvars,as.double(x),ty,tevent,offset,weights,jd,vp,cl,ne,nx,nlam,flmin,ulam,thresh,
-              maxit,isd,# need to get JHF to reverse these
-              lmu=integer(1),
-              ca=double(nx*nlam),
-              ia=integer(nx),
-              nin=integer(nlam),
-              nulldev=double(1),  
-              dev=double(nlam),
-              alm=double(nlam),
-              nlp=integer(1),
-              jerr=integer(1),PACKAGE="glmnetPlus"
-              )
+  else {
+    dotCall64::.C64("coxnet",
+                    SIGNATURE = c("double", "integer", "integer", "double", "double", "double",
+                                  "double", "double", "integer", "double", "double", "integer", "integer",
+                                  "integer", "double", "double", "double", "integer", "integer",
+                                  "integer", "double", "integer", "integer", "double",
+                                  "double", "double", "integer", "integer"),
+                    parm=alpha, nobs, nvars, as.double(x), ty, tevent,
+                    offset, weights, jd, vp, cl, ne, nx,
+                    nlam, flmin, ulam, thresh, maxit, isd,# need to get JHF to reverse these
+                    lmu=integer(1), ca=double(nx*nlam), ia=integer(nx), nin=integer(nlam), nulldev=double(1),
+                    dev=double(nlam), alm=double(nlam), nlp=integer(1), jerr=integer(1),
+                    INTENT = c(rep("r", 19), rep("w", 9)),
+                    PACKAGE="glmnetPlus"
+                    )
+  }
+  # else .Fortran("coxnet",
+  #             parm=alpha,nobs,nvars,as.double(x),ty,tevent,offset,weights,jd,vp,cl,ne,nx,nlam,flmin,ulam,thresh,
+  #             maxit,isd,# need to get JHF to reverse these
+  #             lmu=integer(1),
+  #             ca=double(nx*nlam),
+  #             ia=integer(nx),
+  #             nin=integer(nlam),
+  #             nulldev=double(1),
+  #             dev=double(nlam),
+  #             alm=double(nlam),
+  #             nlp=integer(1),
+  #             jerr=integer(1),PACKAGE="glmnetPlus"
+  #             )
  if(fit$jerr!=0){
   errmsg=jerr(fit$jerr,maxit,pmax=nx,family="cox")
   if(errmsg$fatal)stop(errmsg$msg,call.=FALSE)
